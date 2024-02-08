1e69a0f55610f00eb2da8d052e2cee1419a09d7f
Joris Lodewijks <info@jorislodewijks.nl>
2024-02-08 16:53:51 +0100

    chore: initialized Electron+React+Typescript+Tailwind

---
+++ src/renderer/App.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./App.css";
+
+const root = createRoot(document.getElementById("root"));
+
+export default function App() {
+  return (
+    <div className="h-screen flex flex-col text-center justify-center">
+      <h2 className="text-5xl font-light">👋🏻 Hello from React!</h2>
+    </div>
+  );
+}
+
+root.render(<App />);

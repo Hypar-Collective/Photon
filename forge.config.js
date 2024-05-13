bdee69083bdeabe14f43cb06c0b280f102ba1ec0
Joris Lodewijks <info@jorislodewijks.nl>
2024-05-13 21:52:01 +0200

    fix: add publisher

--- forge.config.js
+++ forge.config.js
@@ -24,6 +24,18 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "hypar-collective",
+          name: "photon",
+        },
+        prerelease: true,
+      },
+    },
+  ],
   plugins: [
     {
       name: "@electron-forge/plugin-auto-unpack-natives",

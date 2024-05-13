13f3f935901447308b877b18eef1e420225f149a
Joris Lodewijks <info@jorislodewijks.nl>
2024-05-13 22:29:20 +0200

    fix: change config

--- forge.config.ts
+++ forge.config.ts
@@ -3,6 +3,7 @@ import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
+import { PublisherGithub } from "@electron-forge/publisher-github";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
@@ -20,6 +21,15 @@ const config: ForgeConfig = {
     new MakerRpm({}),
     new MakerDeb({}),
   ],
+  // publishers: [
+  //   new PublisherGithub({
+  //     repository: {
+  //       owner: "hypar-collective",
+  //       name: "photon",
+  //     },
+  //     prerelease: true,
+  //   }),
+  // ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
